26f3ceb27aa62115750558108e1b226e37dcd42a
alviancode <alvianwijaya200@gmail.com>
2021-10-02 03:19:52 +0700

    Coconut 🥥
    The coconut nut is a giant nut.

--- src/App.js
+++ src/App.js
@@ -20,6 +20,25 @@ function onSceneReady(scene) {
   var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
   light.intensity = 0.7;
 
+  var spriteManagerTrees = new BABYLON.SpriteManager("treesManager", "objects/palmtree.png", 2000, { width: 512, height: 1024 }, scene);
+
+  //We create trees at random positions
+  for (let i = 0; i < 500; i++) {
+    const tree = new BABYLON.Sprite("tree", spriteManagerTrees);
+    tree.position.x = Math.random() * (-30);
+    tree.position.z = Math.random() * 20 + 8;
+    tree.position.y = 0.5;
+  }
+
+  for (let i = 0; i < 500; i++) {
+    const tree = new BABYLON.Sprite("tree", spriteManagerTrees);
+    tree.position.x = Math.random() * (25) + 7;
+    tree.position.z = Math.random() * -35 + 8;
+    tree.position.y = 0.5;
+  }
+
+
+
   var skybox = BABYLON.MeshBuilder.CreateBox("skybox", { size: 150 }, scene);
   var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
   skyboxMaterial.backFaceCulling = false;
